412ff20e32713492f0938896df13fd8591bc6886
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2024-12-05 18:56:42 -0800

    Add more tracked buffs
    Also added quick and dirty fix for karazhan staff bonus healing ruining the heal cache

--- Settings.lua
+++ Settings.lua
@@ -125,12 +125,13 @@ do
     local defaultTrackedBuffs = {
         "Blessing of Protection", "Hand of Protection", "Divine Protection", "Divine Shield", "Divine Intervention", -- Paladin
             "Bulwark of the Righteous",
-        "Power Infusion", "Spirit of Redemption", -- Priest
+        "Power Infusion", "Spirit of Redemption", "Abolish Disease", -- Priest
         "Shield Wall", -- Warrior
         "Evasion", "Vanish", -- Rogue
         "Deterrence", "Feign Death", "Mend Pet", -- Hunter
-        "Frenzied Regeneration", "Innervate", -- Druid
+        "Frenzied Regeneration", "Innervate", "Abolish Poison", -- Druid
         "Soulstone Resurrection", "Hellfire", -- Warlock
+        "Ice Block", "Evocation", -- Mage
         "Quel'dorei Meditation", "Grace of the Sunwell", -- Racial
         "First Aid", "Food", "Drink" -- Generic
     }
@@ -142,11 +143,12 @@ do
             "Blessing of Freedom", "Hand of Freedom", "Redoubt", "Holy Shield"},
         ["PRIEST"] = {"Prayer of Fortitude", "Power Word: Fortitude", "Prayer of Spirit", "Divine Spirit", 
             "Prayer of Shadow Protection", "Shadow Protection", "Holy Champion", "Champion's Grace", "Empower Champion", 
-            "Fear Ward", "Inner Fire", "Power Word: Shield", "Renew", "Lightwell Renew", "Inspiration", "Abolish Disease", 
+            "Fear Ward", "Inner Fire", "Power Word: Shield", "Renew", "Lightwell Renew", "Inspiration", 
             "Fade", "Spirit Tap"},
+        ["WARRIOR"] = {"Battle Shout"},
         ["DRUID"] = {"Gift of the Wild", "Mark of the Wild", "Thorns", "Rejuvenation", "Regrowth"},
         ["SHAMAN"] = {"Water Walking", "Healing Way", "Ancestral Fortitude"},
-        ["MAGE"] = {"Arcane Brilliance", "Arcane Intellect", "Evocation"},
+        ["MAGE"] = {"Arcane Brilliance", "Arcane Intellect", "Frost Armor", "Ice Armor", "Mage Armor", "Ice Barrier"},
         ["WARLOCK"] = {"Demon Armor", "Demon Skin", "Unending Breath", "Shadow Ward", "Fire Shield"},
         ["HUNTER"] = {"Rapid Fire", "Quick Shots", "Quick Strikes", "Aspect of the Pack", 
             "Aspect of the Wild", "Bestial Wrath", "Feed Pet Effect"}
